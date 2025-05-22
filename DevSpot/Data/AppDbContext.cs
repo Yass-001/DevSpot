01fe4f95c8e6460906cfbb48c8261eaa556b3642
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-05-22 14:17:56 +0300

    app v.0.1

--- DevSpot/Data/AppDbContext.cs
+++ DevSpot/Data/AppDbContext.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSpot.Data
 {
     public class AppDbContext : IdentityDbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
     }
 
 }
--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -1,3 +1,6 @@
+using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DevSpot
 {
     public class Program
@@ -5,6 +8,10 @@ namespace DevSpot
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("Database")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
