463dc2e6703f026a273d84d7f5f9fe758d02c4e6
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-05-28 11:26:03 +0300

    app v.0.5.6

---
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,25 @@
+﻿using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSpot.Tests
+{
+    internal class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "DevSpotTestDb")
+                .Options;
+                    }
+
+        private AppDbContext CreateContext() => new AppDbContext(_options);     
+
+    }
+}
