240f2cbe611d6472a6c5c8298e6d4d22770febc3
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-05-28 10:35:18 +0300

    app v.0.5.1

--- DevSpot/Repositiries/JobPostingRepository.cs
+++ DevSpot/Repositiries/JobPostingRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DevSpot.Data;
 using DevSpot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSpot.Repositiries
 {
@@ -19,24 +20,51 @@ namespace DevSpot.Repositiries
 
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            //await _context.JobPostings
+            //    .Where(jp => jp.Id == id)
+            //    .ExecuteDeleteAsync();
+            var jobPosting = await _context.JobPostings.FindAsync(id);
+            if (jobPosting == null)
+            {
+                throw new KeyNotFoundException($"JobPosting with ID {id} not found.");
+            }
+            _context.JobPostings.Remove(jobPosting);
+            await _context.SaveChangesAsync();
+
         }
 
-        public Task<IEnumerable<JobPosting>> GetAllAsync()
+        public async Task<IEnumerable<JobPosting>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            // Use AsNoTracking for better performance if entities are not being updated
+            List<JobPosting> list = await _context.JobPostings
+                .AsNoTracking()
+                .Include(jp => jp.User)
+                .ToListAsync();
+            return list;
         }
 
-        public Task<JobPosting> GetByIdAsync(int id)
+        public async Task<JobPosting> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            JobPosting jp = await _context.JobPostings
+                .Include(jp => jp.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(jp => jp.Id == id);
+            return jp;
         }
 
-        public Task UpdateAsync(JobPosting entity)
+        public async Task UpdateAsync(JobPosting entity)
         {
-            throw new NotImplementedException();
+            await _context.JobPostings
+                .Where(jp => jp.Id == entity.Id)
+                .ExecuteUpdateAsync(jp => jp
+                    .SetProperty(jp => jp.Title, entity.Title)
+                    .SetProperty(jp => jp.Description, entity.Description)
+                    .SetProperty(jp => jp.Company, entity.Company)
+                    .SetProperty(jp => jp.Location, entity.Location)
+                    .SetProperty(jp => jp.IsApproved, entity.IsApproved));
+            await _context.SaveChangesAsync();
         }
     }
 }
