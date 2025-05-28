c68901558a7dd27b9ba90dfdd0307a6532e7dc13
Yuriy Kutsenko <yasssin3008@gmail.com>
2025-05-28 11:03:19 +0300

    app v.0.5.3

--- DevSpot/Repositiries/JobPostingRepository.cs
+++ DevSpot/Repositiries/JobPostingRepository.cs
@@ -26,44 +26,55 @@ namespace DevSpot.Repositiries
             //    .Where(jp => jp.Id == id)
             //    .ExecuteDeleteAsync();
             var jobPosting = await _context.JobPostings.FindAsync(id);
-            if (jobPosting == null)
+            if (jobPosting != null)
+            {
+                _context.JobPostings.Remove(jobPosting);
+                await _context.SaveChangesAsync();
+            }
+            else
             {
                 throw new KeyNotFoundException($"JobPosting with ID {id} not found.");
             }
-            _context.JobPostings.Remove(jobPosting);
-            await _context.SaveChangesAsync();
-
         }
 
         public async Task<IEnumerable<JobPosting>> GetAllAsync()
         {
             // Use AsNoTracking for better performance if entities are not being updated
-            List<JobPosting> list = await _context.JobPostings
-                .AsNoTracking()
-                .Include(jp => jp.User)
-                .ToListAsync();
-            return list;
+            return await _context.JobPostings.ToListAsync();
+
+            //List<JobPosting> list = await _context.JobPostings
+            //    .AsNoTracking()
+            //    .Include(jp => jp.User)
+            //    .ToListAsync();
+            //return list;
         }
 
         public async Task<JobPosting> GetByIdAsync(int id)
         {
-            JobPosting jp = await _context.JobPostings
-                .Include(jp => jp.User)
-                .AsNoTracking()
-                .FirstOrDefaultAsync(jp => jp.Id == id);
-            return jp;
+            var jopPosting = await _context.JobPostings.FindAsync(id);
+            if (jopPosting == null)
+            {
+                throw new KeyNotFoundException($"JobPosting with ID {id} not found.");
+            }
+
+            //JobPosting jp = await _context.JobPostings
+            //    .Include(jp => jp.User)
+            //    .AsNoTracking()
+            //    .FirstOrDefaultAsync(jp => jp.Id == id);
+            return jopPosting;
         }
 
         public async Task UpdateAsync(JobPosting entity)
         {
-            await _context.JobPostings
-                .Where(jp => jp.Id == entity.Id)
-                .ExecuteUpdateAsync(jp => jp
-                    .SetProperty(jp => jp.Title, entity.Title)
-                    .SetProperty(jp => jp.Description, entity.Description)
-                    .SetProperty(jp => jp.Company, entity.Company)
-                    .SetProperty(jp => jp.Location, entity.Location)
-                    .SetProperty(jp => jp.IsApproved, entity.IsApproved));
+            _context.JobPostings.Update(entity);
+            //await _context.JobPostings
+            //    .Where(jp => jp.Id == entity.Id)
+            //    .ExecuteUpdateAsync(jp => jp
+            //        .SetProperty(jp => jp.Title, entity.Title)
+            //        .SetProperty(jp => jp.Description, entity.Description)
+            //        .SetProperty(jp => jp.Company, entity.Company)
+            //        .SetProperty(jp => jp.Location, entity.Location)
+            //        .SetProperty(jp => jp.IsApproved, entity.IsApproved));
             await _context.SaveChangesAsync();
         }
     }
